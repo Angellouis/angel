e6587b49bfb2d4316447b731482a9dab71aeb1b6
ANGEL LOUIS <47616@fisat>
2018-04-18 04:56:10 -0400

    create a function to add 2 3 and 4 numbers

--- add.py
+++ add.py
@@ -4,9 +4,15 @@ def add2(n1, n2):
 
 		print add2(3, 4)
 	
-def add3(n1, n2):
+def add3(n1, n2 ,n3):
 			s = n1 + n2 +n3
 			return s
 
 		print add2(3, 4, 6)
+	
+def add3(n1, n2,n3 ,n4):
+			s = n1 + n2 + n3 + n4
+			return s
+
+		print add2(3, 4, 6, 2)
 	
